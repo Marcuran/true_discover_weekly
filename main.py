76a86e0ef68bd60d46fa5ad29fd61069f0d7ba6a
Marcuran <43537946+Marcuran@users.noreply.github.com>
2023-10-28 10:58:02 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from spotify_api_interface import get_token, create_and_populate_playlist, get_all_artists_listenned_to, get_user_href, create_track_list
+import json
+from dotenv import dotenv_values
+from dotenv import load_dotenv
+import os
+
+
+
+def main():
+    # Load environment variables from the .env file
+    load_dotenv()
+
+    # Access the client ID from the environment
+    client_id = os.getenv("CLIENT_ID")
+    if client_id:
+        print(f"Developer Client ID: {client_id}")
+    else:
+        print("Client ID not found in the .env file.")
+        exit()
+    #TODO reduce the scope given to get_token function to only get what we need 
+    access_token = get_token(client_id, 'user-read-private, user-read-email, playlist-modify-public, playlist-modify-private, playlist-read-private, playlist-read-collaborative, user-top-read, user-read-recently-played, user-library-modify, user-library-read')
+    user_href = get_user_href(access_token)
+    all_artists = get_all_artists_listenned_to(access_token)
+    track_list = create_track_list(access_token, all_artists)
+    create_and_populate_playlist(access_token, user_href, track_list,playlist_name="True discover weekly", playlist_description="get truly never heard before music for you!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ spotify_api_interface.py
@@ -0,0 +1,352 @@
+import random
+import string
+import urllib.parse as urlparse
+from urllib.parse import parse_qs, urlencode
+import hashlib
+import base64
+import requests
+import time
+import random
+import json
+
+LENGTH = 16
+code_verifier = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=128))
+authorization_code = None
+
+def get_token(client_id: str, scope: str, redirect_uri='http://localhost:8888/callback'):
+#TODO keep access token locally and try to use it, if it is not valid anymore ask for a new one, otherwise use it for the rest of the process 
+    """
+    Generates an authorization URL and prompts the user to input the full URL with the authorization code.
+    Retrieves an access token using the authorization code.
+    
+    Args:
+        client_id (str): Client ID obtained from the Spotify Developer Dashboard.
+        scope (str): Scopes required for accessing Spotify API endpoints.
+        redirect_uri (str): Redirect URI specified in the Spotify Developer Dashboard.
+        
+    Returns:
+        str: Access token if the authorization is successful, None otherwise.
+    """
+    global authorization_code
+
+    letters_and_digits = string.ascii_letters + string.digits
+    state = ''.join(random.choice(letters_and_digits) for _ in range(LENGTH))
+    code_verifier_storage = code_verifier
+    code_challenge = base64.urlsafe_b64encode(hashlib.sha256(code_verifier.encode()).digest()).decode().rstrip('=')
+
+    args = {
+        'response_type': 'code',
+        'client_id': client_id,
+        'scope': scope,
+        'redirect_uri': redirect_uri,
+        'state': state,
+        'code_challenge_method': 'S256',
+        'code_challenge': code_challenge
+    }
+
+    query_string = urlencode(args)
+    authorize_url = 'https://accounts.spotify.com/authorize?' + query_string
+
+    print(authorize_url)
+
+    authorization_url = input("Enter the full authorization URL: ")
+    parsed_url = urlparse.urlparse(authorization_url)
+    query_params = parse_qs(parsed_url.query)
+    authorization_code = query_params.get('code', [None])[0]
+
+    if not authorization_code:
+        print("Authorization code not found in the URL.")
+        return None
+
+    token_url = 'https://accounts.spotify.com/api/token'
+
+    token_params = {
+        'grant_type': 'authorization_code',
+        'code': authorization_code,
+        'redirect_uri': redirect_uri,
+        'client_id': client_id,
+        'code_verifier': code_verifier_storage
+    }
+
+    response = requests.post(token_url, data=token_params)
+    response_data = response.json()
+
+    access_token = response_data.get('access_token')
+    return access_token
+
+def get_user_href(access_token):
+    """
+    Retrieves the user's href (Spotify API endpoint) using the access token.
+    
+    Args:
+        access_token (str): Access token for authenticating API requests.
+        
+    Returns:
+        str: User's href (Spotify API endpoint).
+    """
+    private_info_url = 'https://api.spotify.com/v1/me'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    response = requests.get(private_info_url, headers=headers)
+    response_data = response.json()
+    return response_data['href']
+
+def create_and_populate_playlist(access_token, user_href, tracks, playlist_name="Hello world!", playlist_description="Hello world!", public=False):
+    # Create the playlist
+    playlist_id = create_playlist(access_token, user_href, playlist_name, playlist_description, public)
+    
+    if playlist_id:
+        # Add tracks to the created playlist
+        add_tracks_url = f'{user_href}/playlists/{playlist_id}/tracks'
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+            'Content-Type': 'application/json'
+        }
+        track_ids = [track["id"] for track in tracks]
+        track_uris = [f"spotify:track:{track_id}" for track_id in track_ids]
+        
+        if len(track_uris) > 100:
+            track_uris = track_uris[0:100]
+        track_data = {
+            "uris": track_uris
+        }
+        response = requests.post(add_tracks_url, headers=headers, json=track_data)
+        
+        if response.status_code == 201:
+            print("Tracks added to the playlist successfully")
+        else:
+            print("Failed to add tracks to the playlist")
+            print("Response:", response.text)
+    else:
+        print("Failed to create the playlist")
+
+def create_playlist(access_token, user_href, playlist_name="Hello world!", playlist_description="Hello world!", public=False):
+    """
+    Creates a playlist using the user's href and returns the playlist ID.
+    
+    Args:
+        access_token (str): Access token for authenticating API requests.
+        playlist_name (str, optional): Name of the playlist. Defaults to "Hello world!".
+        playlist_description (str, optional): Description of the playlist. Defaults to "Hello world!".
+        public (bool, optional): Indicates if the playlist should be public or not. Defaults to False.
+        
+    Returns:
+        str: Playlist ID of the created playlist.
+    """
+    playlist_creation_url = f'{user_href}/playlists'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+    playlist_info = {
+        "name": playlist_name,
+        "description": playlist_description,
+        "public": public
+    }
+    response = requests.post(playlist_creation_url, headers=headers, json=playlist_info)
+    response_data = response.json()
+    if response.status_code == 201:
+        response_data = response.json()
+    else:
+        # Request failed
+        print("Request failed with status code:", response.status_code)
+        print(response.text)
+        print("didn't work :(")
+        if response.status_code == 429:
+            print("sleeping  30s because we get to many requests error")
+            time.sleep(30)
+    return response_data['id']
+
+def get_user_playlist(access_token, offset, limit=20):
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    params = {
+        'limit': limit,
+        'offset': offset
+    }
+    url = f'https://api.spotify.com/v1/me/playlists'
+    response = requests.get(url, headers=headers, params=params)
+    response_data = response.json()
+    return response_data.get('items', [])
+
+def get_user_items(access_token, item_type, limit=20, total_limit=10000):
+    """
+    Retrieves the user's top items (tracks or artists) with pagination support.
+    
+    Args:
+        access_token (str): Access token for authenticating API requests.
+        user_href (str): User's Spotify API endpoint.
+        item_type (str): Type of items to retrieve ('tracks' or 'artists').
+        limit (int, optional): The maximum number of items to retrieve per request. Defaults to 20.
+        total_limit (int, optional): The maximum number of total items to retrieve. Defaults to 10000.
+    
+    Returns:
+        list: A list of dictionaries representing the user's top items.
+    """
+    unique_item_ids = set()
+    unique_items = []
+
+    time_ranges = ["short_term", "medium_term", "long_term"]
+    for time_range in time_ranges:
+        for offset in range(0, total_limit, limit):
+            items = get_user_items_page(access_token, item_type, limit, offset, time_range)
+            for item in items:
+                item_id = item['id']
+                if item_id not in unique_item_ids:
+                    unique_item_ids.add(item_id)
+                    unique_items.append(item)
+            
+            if len(items) < limit:
+                break
+    
+    return unique_items
+
+def get_user_items_page(access_token, item_type, limit, offset, time_range="medium_term"):
+    if item_type in ["tracks","artists"]: 
+        params = {
+            'limit': limit,
+            'offset': offset,
+            'time_range': time_range
+        }
+        url = f'https://api.spotify.com/v1/me/top/{item_type}'
+    elif item_type == "playlists":
+        params = {
+            'limit': limit,
+            'offset': offset,
+        }
+        url = f'https://api.spotify.com/v1/me/{item_type}'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    response = requests.get(url, headers=headers, params=params)
+    response_data = response.json()
+    return response_data.get('items', [])
+
+def get_all_tracks_from_playlists(access_token, playlists):
+    unique_tracks = []
+    unique_track_ids = []
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    for playlist in playlists:
+        href = playlist["tracks"]["href"]
+        response = requests.get(href,headers=headers)
+        response_data = response.json()
+        tracks =  response_data.get('items',[])
+        for track in tracks:
+            track_id = track['track']['id']
+            if track_id not in unique_track_ids:
+                unique_track_ids.append(track_id)
+                unique_tracks.append(track['track'])
+    return unique_tracks
+
+def get_all_artists_listenned_to(access_token):
+    print("getting all top tracks ...")
+    all_top_tracks = get_user_items(access_token, "tracks")
+    print("getting all top artists ...")
+    all_top_artists = get_user_items(access_token, "artists")
+    print("getting all playlists ...")
+    all_playlists = get_user_items(access_token, "playlists")
+    print("getting all playlist tracks ...")
+    all_playlists_tracks = get_all_tracks_from_playlists(access_token, all_playlists)
+    unique_artists_ids = set()
+    merged_artists = []
+
+
+    for artist in all_top_artists:
+        artist_id = artist['id']
+        if artist_id not in unique_artists_ids:
+            unique_artists_ids.add(artist_id)
+            merged_artists.append(artist)
+
+    for track in all_top_tracks:
+        for artist in track.get('artists', []):
+            artist_id = artist['id']
+            if artist_id not in unique_artists_ids:
+                unique_artists_ids.add(artist_id)
+                merged_artists.append(artist)
+    
+    for track in all_playlists_tracks:
+        for artist in track.get('artists', []):
+            artist_id = artist['id']
+            if artist_id not in unique_artists_ids:
+                unique_artists_ids.add(artist_id)
+                merged_artists.append(artist)
+
+    print("number of individual_artists ", len(merged_artists))
+    return merged_artists
+
+def get_recommendation_from_genre_and_artist(access_token, genre, artist):
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    params= {}
+    params["seed_artists"] = artist
+    params["seed_genres"] = genre
+    params["limit"] = 5
+
+    response = requests.get('https://api.spotify.com/v1/recommendations', headers=headers, params=params)
+    
+
+    if response.status_code == 200:
+        response_data = response.json()
+        recommended_tracks = response_data
+        return recommended_tracks["tracks"]
+    else:
+        # Request failed
+        print("Request failed with status code:", response.status_code)
+        print(response.text)
+        print("didn't work :(")
+        if response.status_code == 429:
+            print("sleeping  30s because we get to many requests error")
+            time.sleep(30)
+    return []
+
+def create_track_list(access_token, all_artists, length=100):
+    genres_dict = {}
+    with open("all_artists.txt", "w") as file:
+        json.dump(all_artists,file)
+    for artist in all_artists:
+        if 'genres' in artist and artist['genres']:
+            for genre in artist['genres']:
+                if genre not in genres_dict.keys():
+                    genres_dict[genre] = [artist['id']]
+                else:
+                    genres_dict[genre].append(artist['id'])
+    all_artists_ids = [artist["id"] for artist in all_artists]
+    print("number of music genres ", len(genres_dict))
+    print("number of artist ids ", len(all_artists_ids))
+
+    track_list = []
+    artists_in_track_list_already =[]
+    print("cooking playlist ... (it will take at least 100s)")
+    for _ in range(50):
+        random_genre = random.choice(list(genres_dict.keys()))
+        random_artist = random.choice(genres_dict[random_genre])
+        recommended_tracks = get_recommendation_from_genre_and_artist(access_token, random_genre, random_artist)
+        for track in recommended_tracks:
+            recommended_artist_ids_in_track = [artist_in_track["id"] for artist_in_track in track["artists"]]
+            add_track = True
+            for recommended_artist_id_in_track in recommended_artist_ids_in_track:
+                if recommended_artist_id_in_track in artists_in_track_list_already:
+                    add_track = False
+                    print (track["name"], " not added to list because artist already in tracklist")
+                elif recommended_artist_id_in_track in all_artists_ids:
+                    add_track = False
+                    print (track["name"], " not added to list because artist already in list of listened artists")
+                elif track in track_list:
+                    add_track = False
+                    print (track["name"], " not added to list because track already in track list")
+            if add_track:
+                track_list.append(track)
+                for artist_id in recommended_artist_ids_in_track :
+                    artists_in_track_list_already.append(artist_id)
+                # print (track["name"], " added to list")
+
+        if len(track_list) > length: 
+            break
+        time.sleep(5)
+    print("cooking finished :)")
+    return track_list
